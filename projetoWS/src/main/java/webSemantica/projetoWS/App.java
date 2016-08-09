55928b1717a54168f20c41abefd9dd67777ce363
monica.silva@srs.ifmt.edu.br <monica.silva@srs.ifmt.edu.br>
2016-08-09 18:01:07 -0400

    Projeto Inicial

---
+++ projetoWS/src/main/java/webSemantica/projetoWS/App.java
@@ -0,0 +1,13 @@
+package webSemantica.projetoWS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
